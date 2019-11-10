802e757299d39bb678d016e6d51a1c334bbcb7aa
zhang <1259558183@qq.com>
2019-11-10 18:49:50 +0800

    update

---
+++ src/base/code/config.js
@@ -0,0 +1,14 @@
+var Config = {
+    mailConfig : {
+        appid: 'your mail appid',
+        appkey: 'your mail appkey',
+        signtype: 'normal'
+    },
+    messageConfig : {
+        appid : '11448',//
+        appkey : 'ffd3551e68cd4866f98ba9c1a9328252',//
+        signtype : 'normal'
+    }
+};
+
+module.exports = Config;
--- src/components/Reg/index.jsx
+++ src/components/Reg/index.jsx
@@ -2,19 +2,19 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import './index.css'
-import SignAndReg from '../../base/SignAndreg'
+import SignAndReg from '../../base/SignAndreg/index'
 import { message, Button } from 'antd';
 
 let tip =''
 const success = () => {
     const hide = message.loading(tip, 0);
-    // Dismiss manually and asynchronously
     setTimeout(hide, 1500);
   };
-  
+
+  let time; //全局声明 定时器
+
 
 export default class  Reg extends React.Component {
-
     constructor(props) {
         super(props)
         this.state = {
@@ -23,77 +23,75 @@ export default class  Reg extends React.Component {
             isShow:false,
             regMsg:'',//提示信息
             signTel:'',//跳转登录数据
+            codeTxt:'获取验证码',
+            code:'',//保存获取到的验证码
         }
     }
 
    componentDidMount(){
-
       //判断是忘记密码还在注册 
        if(this.props.location.search !=="?reg"){
           this.setState({
               jump:'忘记密码',
               isReg:'找回密码',
               isShow:true
-          })
-            
+          })          
        }      
    }
 
-   //跳转登录
-   goSign=()=>{
 
-     this.props.history.push('/sign',{tel:this.state.signTel})
-   }
-
-     
+   
    //注册
    userReg=()=>{
-             //手机号格式验证
+                 //手机号格式验证
        if(new RegExp(/^1[3-9]\d{9}$/).test(this.refs.tel.value)){
            //密码格式验证
             if(new RegExp(/(?!^[a-z]+$)+(?!^[0-9]+$)^[a-z0-9]{6,32}$/i).test(this.refs.pass.value)){
-                   //判断两次密码是否一致
+                 //判断验证码是否正确
+                 if(Number(this.refs.code.value)===Number(this.state.code)){
+                 
+                             //判断两次密码是否一致
                    if(this.refs.pass.value === this.refs.ok.value){
-                       //注册请求
-                            //   form.append("users",this.refs.tel.value)
-                            //   form.append('password',this.refs.pass.value)
-                       fetch('/create',{
-                           method:'post', 
-                           headers: {
-                            "Content-Type":"application/json"
-                            },               
-                           body:JSON.stringify({
-                              users:this.refs.tel.value,
-                              password:this.refs.pass.value,
-                              money:Math.floor(Math.random()*100)
-                            })
-                       }).then(req=>req.json())
-                       .then(data=>{
-                        //    console.log(data)
-                          //判断账号是否存在
-                           if(data.being){
-                            this.setState({
-                                regMsg:"该账号已存在"
-                            })
-                            tip='该账号已存在'
-                            success()
-
-                           }else{
-                            this.setState({
-                                regMsg:"注册成功",
-                                // signTel:this.props.refs.tel.value
-                            })
-                            tip='注册成功'
-                            success()
-                           }
-                       })
-
-                   }else{
-                    this.setState({
-                        regMsg:'密码不一致请确认输入'
-                    }) 
-                   }
+                    //注册请求
                 
+                    fetch('/create',{
+                        method:'post', 
+                        headers: {
+                         "Content-Type":"application/json"
+                         },               
+                        body:JSON.stringify({
+                           users:this.refs.tel.value,
+                           password:this.refs.pass.value,
+                           money:Math.floor(Math.random()*100)
+                         })
+                    }).then(req=>req.json())
+                    .then(data=>{
+                       //判断账号是否存在
+                        if(data.being){
+                         this.setState({
+                             regMsg:"该账号已存在"
+                         })
+                         tip='该账号已存在'
+                         success()
+                        }else{
+                         this.setState({
+                             regMsg:"注册成功",
+                         })
+                         tip='注册成功'
+                         success()
+                        }
+                    })
+                }else{
+                 this.setState({
+                     regMsg:'密码不一致请确认输入'
+                 }) 
+                }
+                 }else{
+                    this.setState({
+                        regMsg:'验证码错误'
+                    })  
+                   
+                 }             
             }else{
                 this.setState({
                     regMsg:'请输入6-16位的数字和密码'
@@ -104,11 +102,62 @@ export default class  Reg extends React.Component {
                regMsg:'你的手机号码格式不正确'
            })
        }
-
    }
 
+   //获取验证码
+ 
+   getCode=()=>{
+     //检测是否输入号
+      if(this.refs.tel.value==''){
+        this.setState({
+            regMsg:'请输入手机号'
+        })
+      }else{
+        let count = 59;
+     time= setInterval(()=>{  //验证码倒计时
+             if(count==0){
+                this.refs.dashed.disabled=false
+                this.setState({
+                    codeTxt:'获取验证码'
+                })
+                count = 60
+                clearInterval(time)
+              
+                
+             }else{
+                 this.refs.dashed.disabled=true
+              
+                 this.setState({
+                     codeTxt:count--
+                 })            
+             }
+         },1000)
 
+             //验证码请求
+       fetch('/code',{
+        method:'post', 
+        headers: {
+                  "Content-Type":"application/json"
+            },
+        body:JSON.stringify({
+             tel:this.refs.tel.value
+        })
+            }).then(req=>req.json())
+        .then(data=>{
+             console.log(data)
+                this.setState({
+                    code:data.code //返回的验证码
+                })
+            })
+      }  
+   }
 
+    //跳转登录
+    goSign=()=>{
+        //跳转时关闭定时器
+        clearInterval(time)
+        this.props.history.push('/sign')
+      }
 
 
     render() {
@@ -120,7 +169,9 @@ export default class  Reg extends React.Component {
                     <p><span className='regtxt'>{this.state.jump}</span><span id="regMsg">{this.state.regMsg}</span></p>
                     <div className="regInp">
                         <div className="tel"><span>手机号码:</span><input type="tel" ref="tel" placeholder="请输入手机号码" /></div>
-                        <div className="code"><span>验证码:</span><input type="text" ref="code" placeholder="请输入验证码" /><button>获取验证码</button></div>
+                              <div className="code"><span>验证码:</span>
+                              <input type="text" ref="code" placeholder="请输入验证码" />
+                              <button onClick={this.getCode}   ref="dashed">{this.state.codeTxt} </button></div>
                         <div className="pass"><span>创建密码:</span><input type="password" ref="pass" placeholder="输入6-16位字母或者数字" /></div>
                         <div className='ok'><span>确认密码:</span><input type="password" ref="ok" placeholder="再次输入密码" /></div>		
                     </div>
